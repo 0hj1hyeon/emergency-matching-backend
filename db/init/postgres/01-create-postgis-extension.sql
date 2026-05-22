932c7e4249ba7b294c25854209f5f4811596b573
ohj1hyeon <ojh010911@gmail.com>
2026-05-22 16:14:34 +0900

    perf: 근처 병원 조회 공간 인덱스 추가

--- db/init/postgres/01-create-postgis-extension.sql
+++ db/init/postgres/01-create-postgis-extension.sql
@@ -1,1 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS postgis;
+
+CREATE INDEX IF NOT EXISTS idx_hospitals_location_geography
+ON hospitals
+USING GIST (
+    ST_SetSRID(ST_MakePoint(longitude, latitude), 4326)::geography
+);
